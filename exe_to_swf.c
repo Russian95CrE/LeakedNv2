5aa751f788fbdfa738480082d4e03cf17d8579e1
Russian95CrE <russian95@enhance11.live>
2024-11-13 21:31:38 -0300

    new exe_to_swf.c code

--- exe_to_swf.c
+++ exe_to_swf.c
@@ -1,79 +1,90 @@
 #include <stdio.h>
 #include <windows.h>
 
+/**
+ * Main function to extract SWF data from a projector bundle.
+ *
+ * @return 0 on success, 1 on failure.
+ */
 int main() {
     // Declare variables for file size, file handle, memory heap, and bytes read/written
-    int     szFile;                       // Size of the input file
-    char    dataEnd[8];                   // Buffer to store the last 8 bytes of the input file
-    DWORD   lpNumberOfBytesRW;            // Number of bytes read/written
-    HANDLE  hfInput,                      // Handle for input file
-            hfOutput,                     // Handle for output file
-            hMemHeap,                     // Handle for allocated heap memory
-            hHeap;                        // Handle to the process heap
-    
-    // Open the input file "Nv2_Current.exe" with read access
-    hfInput = CreateFile("Nv2_Current.exe", GENERIC_READ, 0, NULL, 
-                        OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    int fileSize;                       // Size of the input file
+    char endData[8];                    // Buffer to store the last 8 bytes of the input file
+    DWORD bytesReadOrWritten;           // Number of bytes read/written
+    HANDLE inputFileHandle,             // Handle for input file
+           outputFileHandle,            // Handle for output file
+           heapMemoryHandle,            // Handle for allocated heap memory
+           processHeapHandle;           // Handle to the process heap
+    char fileName[30];                  // Buffer to store the name of the Nv2 file
+
+    // Get the name of the Nv2 file from the user
+    printf("Nv2 file name (with .exe extension): ");
+    scanf("%s", fileName);
+
+    // Open the input file with read access
+    inputFileHandle = CreateFile(fileName, GENERIC_READ, 0, NULL, 
+                                 OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     
     // Check if the file was successfully opened
-    if (hfInput == INVALID_HANDLE_VALUE) {
+    if (inputFileHandle == INVALID_HANDLE_VALUE) {
         printf("Cannot open input file for reading!\r\n");
-    } else {
-        // Get the size of the input file
-        szFile = GetFileSize(hfInput, NULL);
-        
-        // Move the file pointer to 8 bytes before the end of the file
-        SetFilePointer(hfInput, szFile - 8, NULL, FILE_BEGIN);
-        
-        // Read the last 8 bytes of the input file into dataEnd
-        ReadFile(hfInput, dataEnd, 8, &lpNumberOfBytesRW, NULL);
-        
-        // Check if the first 4 bytes of dataEnd match the signature 0xFA123456
-        if (*(DWORD*)&dataEnd == 0xFA123456) { // Check the signature
-            // Get the handle to the process heap
-            hHeap = GetProcessHeap();
-            
-            if (hHeap) {
-                // Allocate heap memory based on the last 4 bytes of dataEnd (file size information)
-                hMemHeap = (PHANDLE)HeapAlloc(hHeap, HEAP_ZERO_MEMORY, *(DWORD*)&dataEnd[4]);
-                
-                // Move the file pointer to the location where the SWF data starts
-                SetFilePointer(hfInput, szFile - 8 - *(DWORD*)&dataEnd[4], NULL, FILE_BEGIN);
-                
-                // Read the SWF data into the allocated memory
-                ReadFile(hfInput, hMemHeap, *(DWORD*)&dataEnd[4], &lpNumberOfBytesRW, NULL);
-                
-                // Create an output file "dumped.swf" to write the extracted data
-                hfOutput = CreateFile("dumped.swf", GENERIC_WRITE, 0, NULL, 
-                                    CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-                
-                // Check if the output file was successfully created
-                if (hfOutput == INVALID_HANDLE_VALUE) {
-                    printf("Cannot create output file for writing!\r\n");
-                } else {
-                    // Write the SWF data from the heap memory to the output file
-                    WriteFile(hfOutput, hMemHeap, *(DWORD*)&dataEnd[4], &lpNumberOfBytesRW, NULL);
-                    
-                    // Close the output file handle
-                    CloseHandle(hfOutput);
-                    
-                    // Output success message
-                    printf("Done, check your shiny new dumped.swf! =)\r\n");
-                }
-                
-                // Free the allocated heap memory
-                HeapFree(hHeap, 0, hMemHeap);
-            } else {
-                printf("Can't fetch the process heap!\r\n");
-            }
-        } else {
-            // If the signature doesn't match, output an error message
-            printf("This doesn't seems to be a projector bundle.\r\n");
-        }
-        
-        // Close the input file handle
-        CloseHandle(hfInput);
+        return 1;
     }
+
+    // Get the size of the input file
+    fileSize = GetFileSize(inputFileHandle, NULL);
+    
+    // Move the file pointer to 8 bytes before the end of the file
+    SetFilePointer(inputFileHandle, fileSize - 8, NULL, FILE_BEGIN);
+    
+    // Read the last 8 bytes of the input file into endData
+    ReadFile(inputFileHandle, endData, 8, &bytesReadOrWritten, NULL);
+    
+    // Check if the first 4 bytes of endData match the signature 0xFA123456
+    if (*(DWORD*)&endData == 0xFA123456) {
+        // Get the handle to the process heap
+        processHeapHandle = GetProcessHeap();
+        
+        if (processHeapHandle) {
+            // Allocate heap memory based on the last 4 bytes of endData (file size information)
+            heapMemoryHandle = (PHANDLE)HeapAlloc(processHeapHandle, HEAP_ZERO_MEMORY, *(DWORD*)&endData[4]);
+            
+            // Move the file pointer to the location where the SWF data starts
+            SetFilePointer(inputFileHandle, fileSize - 8 - *(DWORD*)&endData[4], NULL, FILE_BEGIN);
+            
+            // Read the SWF data into the allocated memory
+            ReadFile(inputFileHandle, heapMemoryHandle, *(DWORD*)&endData[4], &bytesReadOrWritten, NULL);
+            
+            // Create an output file to write the extracted data
+            outputFileHandle = CreateFile("dumped.swf", GENERIC_WRITE, 0, NULL, 
+                                         CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+            
+            // Check if the output file was successfully created
+            if (outputFileHandle == INVALID_HANDLE_VALUE) {
+                printf("Cannot create output file for writing!\r\n");
+            } else {
+                // Write the SWF data from the heap memory to the output file
+                WriteFile(outputFileHandle, heapMemoryHandle, *(DWORD*)&endData[4], &bytesReadOrWritten, NULL);
+                
+                // Close the output file handle
+                CloseHandle(outputFileHandle);
+                
+                // Output success message
+                printf("Done, check your shiny new dumped.swf! =D\r\n");
+            }
+            
+            // Free the allocated heap memory
+            HeapFree(processHeapHandle, 0, heapMemoryHandle);
+        } else {
+            printf("Can't fetch the process heap!\r\n");
+        }
+    } else {
+        // If the signature doesn't match, output an error message
+        printf("This doesn't seem to be a projector bundle.\r\n");
+    }
+    
+    // Close the input file handle
+    CloseHandle(inputFileHandle);
     
     return 0;
 }
